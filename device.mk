5c3b88c0960b335461b658a77e5f3e921d33ec96
Agnes Tachyon <149220283+ruingl@users.noreply.github.com>
2025-11-04 19:03:20 +0800

    Remove bootctrl.ums9230 from build configuration
    Removed bootctrl.ums9230 from PRODUCT_PACKAGES and static boot control HAL.

--- device.mk
+++ device.mk
@@ -19,9 +19,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.ums9230
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.ums9230 \
     libgptutils \
     libz \
