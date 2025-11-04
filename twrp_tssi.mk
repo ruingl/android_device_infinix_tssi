26b29d5a9f89cf292016bb3aa51e8aefa46414bb
Agnes Tachyon <149220283+ruingl@users.noreply.github.com>
2025-11-04 19:02:43 +0800

    Rename product to twrp_tssi and update inheritance

--- twrp_tssi.mk
+++ twrp_tssi.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from tssi device
 $(call inherit-product, device/infinix/tssi/device.mk)
 
 PRODUCT_DEVICE := tssi
-PRODUCT_NAME := omni_tssi
+PRODUCT_NAME := twrp_tssi
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := TSSI
 PRODUCT_MANUFACTURER := infinix
